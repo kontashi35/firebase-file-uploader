9b9d8f376f97ca991550da281fa150bd83092a8e
kontashi35 <khakontas@gmail.com>
2020-01-14 23:35:24 +0530

    added

---
+++ src/app/Constant.ts
@@ -0,0 +1,16 @@
+export class Constant{
+  static names = [
+      'tenzin',
+      'sonam',
+      'gonpa',
+      'dawa',
+      'tsona',
+      'dolma',
+      'tashi',
+      'palmo','choden','sopa','karma','norbu','dhundup','phuntsok','namgyal',
+      'kunchok','dorje','dawa','yankyi','tsering','yangom','anu'
+  ];
+    constructor(){
+        
+    } 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DropzoneDirective } from './dropzone.directive';
+import { UploaderComponent } from './uploader/uploader.component';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+var firebaseConfig = {
+  apiKey: "AIzaSyA1syvbF-LuEK937af2qXuwakWKP2aYOdM",
+  authDomain: "techtibetmusic.firebaseapp.com",
+  databaseURL: "https://techtibetmusic.firebaseio.com",
+  projectId: "techtibetmusic",
+  storageBucket: "techtibetmusic.appspot.com",
+  messagingSenderId: "423225161491",
+  appId: "1:423225161491:web:5454c9c811b4e4e6264ce6"
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropzoneDirective,
+    UploaderComponent,
+    UploadTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
+
   ],
   providers: [],
   bootstrap: [AppComponent]
